ab44beb32f5b9e864adc42d0c182857cb8bce361
alexkar598 <>
2020-06-30 23:44:22 -0400

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,168 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = require("http");
+var https_1 = require("https");
+var node_html_parser_1 = require("node-html-parser");
+var fs_1 = __importDefault(require("fs"));
+var path_1 = __importDefault(require("path"));
+try {
+    fs_1.default.writeFileSync(path_1.default.resolve(__dirname, "../data/data.json"), "{\"version\": null}", { flag: "ax" });
+}
+catch (e) { }
+var data = require("../data/data.json");
+var config = require("../data/config.json");
+function sendMessage(obj) {
+    try {
+        var body_1 = JSON.stringify(obj);
+        config.webhooks.forEach(function (url) {
+            var options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Content-Length": Buffer.byteLength(body_1)
+                }
+            };
+            console.log(obj);
+            var dreq = https_1.request(url, options);
+            dreq.on("error", function (e) {
+                console.error("Error with request: " + e.message);
+            });
+            dreq.on("data", function (res) {
+                res.pipe(process.stdout);
+            });
+            dreq.on("end", function () {
+                debugger;
+            });
+            dreq.write(body_1);
+            dreq.end();
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+var verreq = https_1.request("https://secure.byond.com/download/version.txt", function (res) {
+    var byondver = "";
+    res.setEncoding("utf8");
+    res.on("data", function (chunk) {
+        byondver += chunk;
+    });
+    res.on("end", function () {
+        if (!res.complete) {
+            console.error("Response is not complete");
+            return;
+        }
+        byondver = byondver.trim(); //yeets the newline at the end
+        console.log(byondver);
+        if (data.version === byondver) {
+            console.log("Nothing to do");
+            return;
+        }
+        var req = http_1.request("http://www.byond.com/docs/notes/513.html", function (res) {
+            var content = "";
+            res.setEncoding("utf8");
+            res.on("data", function (chunk) {
+                content += chunk;
+            });
+            res.on("end", function () {
+                if (!res.complete) {
+                    console.error("Response is not complete");
+                    return;
+                }
+                console.time("HTMLParsing");
+                var htmlschema = node_html_parser_1.parse(content /*.replace(/\n/g, "")*/);
+                var children = htmlschema.querySelector("body").removeWhitespace().childNodes;
+                children.shift(); //the first one is just the release notes header
+                children.pop(); //the last one is just the footer for other release notes
+                var builds = [];
+                var currentbuild;
+                var currentcat;
+                var currentprogram;
+                children.forEach(function (value) {
+                    if (value.tagName === "h3") {
+                        if (currentbuild) {
+                            builds.push(currentbuild);
+                        }
+                        currentbuild = {
+                            name: /Build (\d{3}\.\d{4})/.exec(value.text)[1],
+                            categories: []
+                        };
+                    }
+                    if (value.tagName === "p") {
+                        if (value.childNodes.length === 4) {
+                            currentcat = {
+                                name: value.querySelector("u").text,
+                                link: value.querySelector("a").getAttribute("href"),
+                                programs: []
+                            };
+                            currentbuild.categories.push(currentcat);
+                        }
+                        else if (value.childNodes.length === 2) {
+                            currentprogram = {
+                                name: value.childNodes[0].text,
+                                entries: []
+                            };
+                            value.childNodes[1].childNodes.forEach(function (ival) {
+                                var innervalue = ival;
+                                if (innervalue.tagName === "li") {
+                                    //@ts-ignore
+                                    currentprogram.entries.push(innervalue.structuredText);
+                                }
+                            });
+                            currentcat.programs.push(currentprogram);
+                        }
+                    }
+                });
+                console.timeEnd("HTMLParsing");
+                console.log(builds);
+                data.version = byondver;
+                fs_1.default.writeFileSync(path_1.default.resolve(__dirname, "../data/data.json"), JSON.stringify(data));
+                var fields = [
+                /*  {
+                      name: "\u200b",
+                      value: ""
+                  },
+                  {
+                      name: "BYONDexe",
+                      value: "\u25CF owo\n\u25CF uwu"
+                  }*/
+                ];
+                var selectedbuild = builds[0];
+                var embed = {
+                    content: "BYOND version " + selectedbuild.name + " released!",
+                    embeds: [
+                        {
+                            title: "BYOND version " + selectedbuild.name,
+                            url: "http://www.byond.com/download/",
+                            timestamp: new Date().toISOString(),
+                            fields: fields
+                        }
+                    ],
+                    allowed_mentions: {
+                        parse: []
+                    }
+                };
+                selectedbuild.categories.forEach(function (cat) {
+                    fields.push({
+                        name: "\u200b",
+                        value: "**__[" + cat.name + "](" + cat.link + ")__**"
+                    });
+                    cat.programs.forEach(function (prog) {
+                        var entry = "\u25CF " + prog.entries.join("\n\u25CF ");
+                        fields.push({
+                            name: prog.name,
+                            value: entry
+                        });
+                    });
+                });
+                sendMessage(embed);
+            });
+        });
+        req.end();
+    });
+});
+verreq.end();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,193 @@
+import { request } from "http";
+import { request as sreq } from "https";
+import { parse } from "node-html-parser";
+import { HTMLElement } from "node-html-parser"
+import TextNode from "node-html-parser/dist/nodes/text"
+import fs from "fs";
+import path from "path";
+
+try {
+    fs.writeFileSync(path.resolve(__dirname, "../data/data.json"), "{\"version\": null}", {flag: "ax"});
+}catch(e) {}
+
+const data = require("../data/data.json");
+const config = require("../data/config.json")
+
+function sendMessage(obj: object){
+    try {
+        const body = JSON.stringify(obj);
+        config.webhooks.forEach((url: string) => {
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Content-Length": Buffer.byteLength(body)
+                }
+            };
+            console.log(obj)
+            const dreq = sreq(url, options);
+
+            dreq.on("error", (e) => {
+                console.error(`Error with request: ${e.message}`);
+            });
+            dreq.on("data", res => {
+                res.pipe(process.stdout)
+            });
+            dreq.on("end", () => {
+                debugger
+            })
+            dreq.write(body);
+            dreq.end();
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+
+interface build {
+    name: string,
+    categories: category[]
+}
+
+interface category {
+    name: string,
+    link: string,
+    programs: program[]
+}
+
+interface program {
+    name: string,
+    entries: string[]
+}
+
+
+const verreq = sreq("https://secure.byond.com/download/version.txt", res => {
+    let byondver = "";
+    res.setEncoding("utf8");
+    res.on("data", chunk => {
+        byondver += chunk;
+    })
+    res.on("end", () => {
+        if (!res.complete) {
+            console.error("Response is not complete");
+            return;
+        }
+        byondver = byondver.trim() //yeets the newline at the end
+        console.log(byondver)
+
+        if(data.version === byondver) {
+            console.log("Nothing to do");
+            return
+        }
+
+        const req = request("http://www.byond.com/docs/notes/513.html", res => {
+            let content = "";
+            res.setEncoding("utf8");
+            res.on("data", chunk => {
+                content += chunk;
+            })
+            res.on("end", () => {
+                if (!res.complete) {
+                    console.error("Response is not complete");
+                    return;
+                }
+                console.time("HTMLParsing")
+                const htmlschema = parse(content/*.replace(/\n/g, "")*/);
+
+                const children = htmlschema.querySelector("body").removeWhitespace().childNodes as HTMLElement[];
+
+                children.shift(); //the first one is just the release notes header
+                children.pop(); //the last one is just the footer for other release notes
+
+                const builds: Array<build> = []
+                let currentbuild: build
+                let currentcat: category
+                let currentprogram: program
+                children.forEach(value => {
+                    if(value.tagName === "h3") {
+                        if(currentbuild) {
+                            builds.push(currentbuild as build)
+                        }
+                        currentbuild = {
+                            name: /Build (\d{3}\.\d{4})/.exec(value.text)![1],
+                            categories: []
+                        }
+                    }
+                    if(value.tagName === "p") {
+                        if(value.childNodes.length === 4) {
+                            currentcat = {
+                                name: value.querySelector("u").text,
+                                link: value.querySelector("a").getAttribute("href")!,
+                                programs: []
+                            }
+                            currentbuild.categories.push(currentcat)
+                        } else if(value.childNodes.length === 2) {
+                            currentprogram = {
+                                name: (value.childNodes[0] as TextNode).text,
+                                entries: []
+                            };
+                            (value.childNodes[1] as HTMLElement).childNodes.forEach((ival) => {
+                                const innervalue = ival as unknown as HTMLElement
+                                if(innervalue.tagName === "li") {
+                                    //@ts-ignore
+                                    currentprogram.entries.push(innervalue.structuredText)
+                                }
+                            })
+
+                            currentcat.programs.push(currentprogram)
+                        }
+                    }
+                })
+                console.timeEnd("HTMLParsing")
+                console.log(builds)
+
+                data.version = byondver
+                fs.writeFileSync(path.resolve(__dirname, "../data/data.json"), JSON.stringify(data))
+
+                const fields: {name: string, value:string}[] = [
+                  /*  {
+                        name: "\u200b",
+                        value: ""
+                    },
+                    {
+                        name: "BYONDexe",
+                        value: "\u25CF owo\n\u25CF uwu"
+                    }*/
+                ]
+                const selectedbuild = builds[0]
+                const embed = {
+                    content: `BYOND version ${selectedbuild.name} released!`,
+                    embeds: [
+                        {
+                            title: `BYOND version ${selectedbuild.name}`,
+                            url: "http://www.byond.com/download/",
+                            timestamp: new Date().toISOString(),
+                            fields
+                        }
+                    ],
+                    allowed_mentions: {
+                        parse: []
+                    }
+                }
+                selectedbuild.categories.forEach(cat => {
+                    fields.push({
+                        name: "\u200b",
+                        value: `**__[${cat.name}](${cat.link})__**`
+                    })
+                    cat.programs.forEach(prog => {
+                        const entry = "\u25CF " + prog.entries.join("\n\u25CF ")
+                        fields.push({
+                            name: prog.name,
+                            value: entry
+                        })
+                    })
+                })
+                sendMessage(embed)
+            })
+        });
+
+        req.end();
+    })
+})
+verreq.end()
